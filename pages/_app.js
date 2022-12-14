bbb8f4626485fb573b673023bd70b1ce91a7eec0
Ronit Kumar Sarkar <ronitkumar.sarkar2019@vitstudent.ac.in>
2022-12-14 17:48:36 +0530

    added background

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,28 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 const colors = {
   brand: {
-    500: "#CB5FFE"
+    500: '#CB5FFE',
   },
-}
+};
 
 function MyApp({ Component, pageProps }) {
-  return
-  <ChakraProvider theme={extendTheme(colors)}>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  return (
+    <ChakraProvider theme={extendTheme(colors)}>
+      <Box
+        bgImage={'/Background.png'}
+        w={'100vw'}
+        h={'100vh'}
+        bgSize={'cover'}
+        bgRepeat={'no-repeat'}
+        bgPosition={'center'}
+      >
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
